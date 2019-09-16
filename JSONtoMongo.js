a69e971f55171cfedddb3cb7b9a6a304e0494a07
Brian Lu <brian.lu@ufl.edu>
2019-09-16 17:08:10 -0400

    fixed the dependencies as well as saving the elements to MongoDB

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -9,10 +9,11 @@ var fs = require('fs'),
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
+mongoose.set('useCreateIndex',true);
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
-
+mongoose.connect(config.db.uri,{useNewUrlParser: true});
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
@@ -20,8 +21,22 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+fs.readFile('listings.json','utf8',function(err,data){
+  if(err){
+    console.error(err);
+    throw(err);
+  }
+  var listingData=JSON.parse(data);
+  var array=listingData.entries;
 
+  array.forEach(function(element){
+    var current=new Listing(element);
+    current.save(function(err){
+      if(err) throw err;
+    });
+  });
 
+});
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
