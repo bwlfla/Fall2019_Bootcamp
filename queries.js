248423534014b6d689b7a9b3009a28742f9228eb
Brian Lu <brian.lu@ufl.edu>
2019-09-17 12:45:36 -0400

    Library West and Remove Cable complete

--- queries.js
+++ queries.js
@@ -1,7 +1,11 @@
 /* Add all the required libraries*/
+var mongoose=require('mongoose'),
+    Listing=require('./ListingSchema'),
+    config=require('./config');
 
+mongoose.set('useCreateIndex',true);
 /* Connect to your database using mongoose - remember to keep your key secret*/
-
+mongoose.connect(config.db.uri,{useNewUrlParser:true});
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
@@ -10,6 +14,10 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   Listing.findOne({name:'Library West'}, function(err, library){
+    if(err) return handleError(err);
+    console.log(library);
+   })
 };
 var removeCable = function() {
   /*
@@ -17,6 +25,13 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   Listing.findOneAndDelete({code:'CABL'},function(err,cable){
+    console.log(cable);
+    if(cable)
+      console.log("Found CABL and deleted");
+    else
+      console.log("Code CABL not found");
+   })
 };
 var updatePhelpsLab = function() {
   /*
@@ -35,5 +50,5 @@ var retrieveAllListings = function() {
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
-retrieveAllListings();
+//updatePhelpsMemorial();
+//retrieveAllListings();
